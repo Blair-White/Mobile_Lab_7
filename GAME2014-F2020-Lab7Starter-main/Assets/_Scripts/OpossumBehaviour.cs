1efed105f75f828bcab427b3cdf22cb59374856f
Blair-White <blairjwhite@hotmail.com>
2020-11-11 18:02:37 -0500

    First Break, enemy with basic move ai.

---
+++ GAME2014-F2020-Lab7Starter-main/Assets/_Scripts/OpossumBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpossumBehaviour : MonoBehaviour
+{
+    public float runForce;
+    public bool isGroundAhead;
+    public Transform lookAheadPoint;
+    public Transform lookBelowPoint;
+    public Rigidbody2D rigidbody2D;
+    public LayerMask colGroundLayer;
+    public LayerMask colWallLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _LookInFront();
+        _LookAhead();
+        _Move();
+    }
+
+    private void _Move()
+    {
+        if(isGroundAhead)
+        {
+            rigidbody2D.AddForce(Vector2.left * runForce * Time.deltaTime * transform.localScale.x);
+            rigidbody2D.velocity *= 0.90f;
+        }
+        else 
+        {
+            _FlipX();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platforms"))
+        {
+            isGroundAhead = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platforms"))
+        {
+            isGroundAhead = false;
+        }
+    }
+
+    private void _LookAhead()
+    {
+        isGroundAhead = Physics2D.Linecast(transform.position, lookBelowPoint.position, colGroundLayer);
+        Debug.DrawLine(transform.position, lookBelowPoint.position, Color.green);
+    }
+
+    private void _LookInFront()
+    {
+        if (isGroundAhead = Physics2D.Linecast(transform.position, lookAheadPoint.position, colWallLayer))
+            _FlipX();
+        Debug.DrawLine(transform.position, lookAheadPoint.position, Color.green);
+    }
+
+    private void _FlipX()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1.0f, transform.localScale.y, transform.localScale.z);
+    }
+}
--- GAME2014-F2020-Lab7Starter-main/Assets/_Scripts/PlayerBehaviour.cs
+++ GAME2014-F2020-Lab7Starter-main/Assets/_Scripts/PlayerBehaviour.cs
@@ -11,6 +11,7 @@ public class PlayerBehaviour : MonoBehaviour
     public float verticalForce;
     public bool isGrounded;
     public bool isJumping;
+    public bool isCrouching;
     public Transform spawnPoint;
 
     private Rigidbody2D m_rigidBody2D;
@@ -26,7 +27,7 @@ public class PlayerBehaviour : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         _Move();
     }
@@ -35,36 +36,50 @@ public class PlayerBehaviour : MonoBehaviour
     {
         if (isGrounded)
         {
-            if (joystick.Horizontal > joystickHorizontalSensitivity)
+            if (!isJumping && !isCrouching)
             {
-                // move right
-                m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
-                m_spriteRenderer.flipX = false;
-                m_animator.SetInteger("AnimState", 1);
-            }
-            else if (joystick.Horizontal < -joystickHorizontalSensitivity)
-            {
-                // move left
-                m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
-                m_spriteRenderer.flipX = true;
-                m_animator.SetInteger("AnimState", 1);
-            }
-            else if(!isJumping)
-            {
-                m_animator.SetInteger("AnimState", 0);
-            }
+                if (joystick.Horizontal > joystickHorizontalSensitivity)
+                {
+                    // move right
+                    m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                    m_spriteRenderer.flipX = false;
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.Run);
+                }
+                else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+                {
+                    // move left
+                    m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                    m_spriteRenderer.flipX = true;
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.Run);
+                }
+                else
+                {
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.Idle);
+                }
 
-
+            }
+            
             if ((joystick.Vertical > joystickVerticalSensitivity) && (!isJumping))
             {
                 // jump
-                m_rigidBody2D.AddForce(Vector2.up * verticalForce * Time.deltaTime);
-                m_animator.SetInteger("AnimState", 2);
+                m_rigidBody2D.AddForce(Vector2.up * verticalForce);
+                m_animator.SetInteger("AnimState", (int)PlayerAnimationType.Jump);
                 isJumping = true;
             }
             else
             {
                 isJumping = false;
+            }
+
+            if ((joystick.Vertical < -joystickVerticalSensitivity) && (!isCrouching))
+            {
+                
+                m_animator.SetInteger("AnimState", (int)PlayerAnimationType.Crouch);
+                isCrouching = true;
+            }
+            else
+            {
+                isCrouching = false;
             }
         }
 
